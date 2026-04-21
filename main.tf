9449ddecf996d9077d440c95ae4764d81f309466
gm276811 <gm276811@fi365.ort.edu.uy>
2026-04-21 01:01:36 +0000

    Parte 3: Infraestructura de red completa (VPC, Subnet, IGW, RT)

--- main.tf
+++ main.tf
@@ -2,39 +2,88 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# 1. Crear el Security Group
+# 1. Crear el VPC
+resource "aws_vpc" "test-terraform-vpc" {
+  cidr_block           = "172.16.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "test-terraform-vpc"
+  }
+}
+
+# 2. Crear la Subnet
+resource "aws_subnet" "test-terraform-subnet" {
+  vpc_id                  = aws_vpc.test-terraform-vpc.id
+  cidr_block              = "172.16.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true # Para que la instancia tenga IP pública
+
+  tags = {
+    Name = "test-terraform-subnet"
+  }
+}
+
+# 3. Crear el Internet Gateway (La puerta a Internet)
+resource "aws_internet_gateway" "test-terraform-ig" {
+  vpc_id = aws_vpc.test-terraform-vpc.id
+
+  tags = {
+    Name = "test-terraform-ig"
+  }
+}
+
+# 4. Crear la Route Table y la Ruta a Internet
+resource "aws_route_table" "test-terraform-rt" {
+  vpc_id = aws_vpc.test-terraform-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.test-terraform-ig.id
+  }
+
+  tags = {
+    Name = "test-terraform-rt"
+  }
+}
+
+# 5. Asociar la Route Table a la Subnet
+resource "aws_route_table_association" "test-terraform-rta" {
+  subnet_id      = aws_subnet.test-terraform-subnet.id
+  route_table_id = aws_route_table.test-terraform-rt.id
+}
+
+# 6. Security Group (AHORA ASOCIADO AL NUEVO VPC)
 resource "aws_security_group" "test-terraform-sg" {
   name        = "test-terraform-sg"
-  description = "Security Group creado con Terraform para SSH"
+  vpc_id      = aws_vpc.test-terraform-vpc.id # ¡CLAVE!
 
-  # Regla de entrada (Ingress): Permitir SSH (puerto 22)
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Permite desde cualquier IP
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Regla de salida (Egress): Permitir todo a cualquier destino
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"        # -1 significa "todos los protocolos"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# 2. Asociar el SG a la instancia
+# 7. Instancia EC2 (AHORA EN LA NUEVA SUBNET)
 resource "aws_instance" "test-terraform-ec2" {
   ami                    = "ami-0ea87431b78a82070"
   instance_type          = "t2.micro"
   key_name               = "vockey"
-
-  # Aquí asociamos el ID del Security Group creado arriba
+  
+  subnet_id              = aws_subnet.test-terraform-subnet.id # ¡CLAVE!
   vpc_security_group_ids = [aws_security_group.test-terraform-sg.id]
 
   tags = {
     Name = "test-terraform-ec2"
   }
 }
-
