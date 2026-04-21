6d3d7f1d9ba47dad6999c5fc7324247f5127bfdf
gm276811 <gm276811@fi365.ort.edu.uy>
2026-04-21 00:55:55 +0000

    Parte 2: Agregado Security Group y asociado a EC2

--- main.tf
+++ main.tf
@@ -2,9 +2,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# 1. Crear el Security Group
+resource "aws_security_group" "test-terraform-sg" {
+  name        = "test-terraform-sg"
+  description = "Security Group creado con Terraform para SSH"
+
+  # Regla de entrada (Ingress): Permitir SSH (puerto 22)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Permite desde cualquier IP
+  }
+
+  # Regla de salida (Egress): Permitir todo a cualquier destino
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"        # -1 significa "todos los protocolos"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 2. Asociar el SG a la instancia
 resource "aws_instance" "test-terraform-ec2" {
-  ami           = "ami-0ea87431b78a82070" # Amazon Linux 2023
-  instance_type = "t2.micro"
+  ami                    = "ami-0ea87431b78a82070"
+  instance_type          = "t2.micro"
+  key_name               = "vockey"
+
+  # Aquí asociamos el ID del Security Group creado arriba
+  vpc_security_group_ids = [aws_security_group.test-terraform-sg.id]
 
   tags = {
     Name = "test-terraform-ec2"
